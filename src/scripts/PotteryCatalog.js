08ed6631eb4cf7723f83995cd2f87f177ea5e022
Kevin Quail <kevinq.7365@gmail.com>
2023-07-28 11:45:09 -0500

    added catalog functon with a copy array function

---
+++ src/scripts/PotteryCatalog.js
@@ -0,0 +1,19 @@
+let forSale = [];
+
+export const toSellOrNotToSell = (pottery) => {
+  if (pottery.cracked === true) {
+    pottery.price = 0;
+    return pottery;
+  } else if (pottery.weight >= 6) {
+    pottery.price = 40;
+  } else if (pottery.weight < 6) {
+    pottery.price = 20;
+  } else pottery.cracked === false;
+  forSale.push(pottery);
+
+  return pottery;
+};
+
+export const usePottery = () => {
+  return forSale;
+};
